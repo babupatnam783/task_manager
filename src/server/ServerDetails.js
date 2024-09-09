d4d79fbdb43bd2980d1e7e7da8c48441140ce57d
babupatnam783 <r170783@rguktrkv.ac.in>
2024-09-09 11:38:48 +0530

    Fix:Addedproductionapiurl

--- src/server/ServerDetails.js
+++ src/server/ServerDetails.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: process.env.REACT_APP_LOCALHOST,
-    // baseURL: process.env.REACT_APP_BASE_URL,
+    // baseURL: process.env.REACT_APP_LOCALHOST,
+    baseURL: process.env.REACT_APP_PRODUCTION,
 });
 export default instance;
